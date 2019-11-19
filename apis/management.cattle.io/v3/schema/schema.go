c1b439aa0f4feae267b1367fb90a1a9797470fe3
hd <18511890109@163.com>
2019-11-19 13:54:55 +0800

    add oam_types(component,container)

--- apis/management.cattle.io/v3/schema/schema.go
+++ apis/management.cattle.io/v3/schema/schema.go
@@ -807,5 +807,8 @@ func etcdBackupTypes(schemas *types.Schemas) *types.Schemas {
 }
 
 func oamTypes(schemas *types.Schemas) *types.Schemas {
-	return schemas.MustImport(&Version, v3.ApplicationConfigurationTemplate{})
+	return schemas.
+	    MustImport(&Version, v3.ApplicationConfigurationTemplate{}).
+	    MustImport(&Version, v3.Component{}).
+	    MustImport(&Version, v3.Container{})
 }
