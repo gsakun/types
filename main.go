7a418507bca326a8b1d7a34718a8df2f3356466b
hd <18511890109@163.com>
2019-12-02 15:43:41 +0800

    fix

--- main.go
+++ main.go
@@ -4,7 +4,7 @@
 package main
 
 import (
-	istioauthnv1alphav1 "github.com/rancher/types/pkg/istio/apis/authentication/v1alpha1"
+	istioauthnv1alphav1 "github.com/lord/types/pkg/istio/apis/authentication/v1alpha1"
 	istiorbacv1alphav1 "github.com/lord/types/pkg/istio/apis/rbac/v1alpha1"
 	monitoringv1 "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1"
 	istiov1alpha3 "github.com/knative/pkg/apis/istio/v1alpha3"
