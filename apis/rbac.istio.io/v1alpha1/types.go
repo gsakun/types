83f4bb98048aa2b08d1fb2e3822ed86b3c5a20f0
hd <18511890109@163.com>
2019-12-02 15:21:19 +0800

    fix

--- apis/rbac.istio.io/v1alpha1/types.go
+++ apis/rbac.istio.io/v1alpha1/types.go
@@ -1,4 +1,4 @@
-package v3
+package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -58,4 +58,3 @@ const (
 	// RBAC engine run policies in PERMISSIVE mode and logs stats.
 	PERMISSIVE EnforcementMode = 1
 )
-
