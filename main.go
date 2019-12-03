21bb8350f4b4d01dc1f591c98e8ea7b071c82cc2
hd <18511890109@163.com>
2019-12-03 16:04:13 +0800

    test generate

--- main.go
+++ main.go
@@ -4,24 +4,24 @@
 package main
 
 import (
-	istioauthnv1alphav1 "github.com/lord/types/pkg/istio/apis/authentication/v1alpha1"
-	istiorbacv1alphav1 "github.com/lord/types/pkg/istio/apis/rbac/v1alpha1"
-	monitoringv1 "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1"
-	istiov1alpha3 "github.com/knative/pkg/apis/istio/v1alpha3"
+	//istioauthnv1alphav1 "github.com/lord/types/pkg/istio/apis/authentication/v1alpha1"
+	//istiorbacv1alphav1 "github.com/lord/types/pkg/istio/apis/rbac/v1alpha1"
+	//monitoringv1 "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1"
+	//istiov1alpha3 "github.com/knative/pkg/apis/istio/v1alpha3"
 	clusterSchema "github.com/rancher/types/apis/cluster.cattle.io/v3/schema"
 	managementSchema "github.com/rancher/types/apis/management.cattle.io/v3/schema"
 	publicSchema "github.com/rancher/types/apis/management.cattle.io/v3public/schema"
 	projectSchema "github.com/rancher/types/apis/project.cattle.io/v3/schema"
 	"github.com/rancher/types/generator"
-	"k8s.io/api/apps/v1beta2"
-	batchv1 "k8s.io/api/batch/v1"
-	batchv1beta1 "k8s.io/api/batch/v1beta1"
+	//"k8s.io/api/apps/v1beta2"
+	//batchv1 "k8s.io/api/batch/v1"
+	//batchv1beta1 "k8s.io/api/batch/v1beta1"
 
-	"k8s.io/api/core/v1"
-	extv1beta1 "k8s.io/api/extensions/v1beta1"
-	knetworkingv1 "k8s.io/api/networking/v1"
-	rbacv1 "k8s.io/api/rbac/v1"
-	k8sschema "k8s.io/apimachinery/pkg/runtime/schema"
+	//"k8s.io/api/core/v1"
+	//extv1beta1 "k8s.io/api/extensions/v1beta1"
+	//knetworkingv1 "k8s.io/api/networking/v1"
+	//rbacv1 "k8s.io/api/rbac/v1"
+	//k8sschema "k8s.io/apimachinery/pkg/runtime/schema"
 )
 
 func main() {
@@ -35,6 +35,7 @@ func main() {
 		"clusterAuthToken":     true,
 	})
 	generator.Generate(projectSchema.Schemas, nil)
+	/*
 	generator.GenerateNativeTypes(v1.SchemeGroupVersion, []interface{}{
 		v1.Endpoints{},
 		v1.PersistentVolumeClaim{},
@@ -114,4 +115,5 @@ func main() {
 		},
 		[]interface{}{},
 	)
+	*/
 }
